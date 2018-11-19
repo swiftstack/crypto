d19fecd42e93bf89f17d422ec96830707d4252f2
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-11-20 02:43:13 +0300

    [Package] Update Hex dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             url: "https://github.com/swift-stack/stream.git",
             .branch("master")),
         .package(
-            url: "https://github.com/swift-stack/hex.git",
+            url: "https://github.com/swift-stack/radix.git",
             .branch("master")),
         .package(
             url: "https://github.com/swift-stack/test.git",
