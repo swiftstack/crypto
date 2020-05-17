4632fa01cf6e183df0ce55d9e551674d06c2f79b
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-17 22:56:04 +0300

    [X509] Refactoring

--- Sources/X509/Name/GeneralName.swift
+++ Sources/X509/Name/GeneralName.swift
@@ -60,43 +60,70 @@ extension GeneralName {
             throw Error.invalidASN1(asn1)
         }
         switch tag {
-        case .otherName:
-            self = .otherName(try .init(from: asn1))
-        case .rfc822Name:
-            guard let string = asn1.stringValue else {
-               throw Error.invalidASN1(asn1)
-            }
-            self = .rfc822Name(string)
-        case .dnsName:
-            guard case .integer(.insane(let bytes)) = asn1.content else {
-                throw Error.invalidASN1(asn1)
-            }
-            self = .dnsName(String(decoding: bytes, as: UTF8.self))
-        case .x400Address:
-            self = .x400Address(try .init(from: asn1))
-        case .directoryName:
-            // TODO: test why this is nested
-            guard let sequence = asn1.sequenceValue, sequence.count == 1 else {
-                throw Error.invalidASN1(asn1)
-            }
-            self = .directoryName(try .init(from: sequence[0]))
-        case .ediPartyName:
-            self = .ediPartyName(try .init(from: asn1))
-        case .uniformResourceIdentifier:
-            guard let string = asn1.stringIdentifierValue else {
-                throw Error.invalidASN1(asn1)
-            }
-            self = .uniformResourceIdentifier(string)
-        case .ipAddress:
-            guard let bytes = asn1.dataValue else {
-               throw Error.invalidASN1(asn1)
-            }
-            self = .ipAddress(bytes)
-        case .registeredId:
-            guard let bytes = asn1.dataValue else {
-               throw Error.invalidASN1(asn1)
-            }
-            self = .registeredId(.init(rawValue: bytes))
+        case .otherName: try self.init(decodingOtherName: asn1)
+        case .rfc822Name: try self.init(decodingRFC822Name: asn1)
+        case .dnsName: try self.init(decodingDNSName: asn1)
+        case .x400Address: try self.init(decodingX400Address: asn1)
+        case .directoryName: try self.init(decodingDirectoryName: asn1)
+        case .ediPartyName: try self.init(decodingEDIPartyName: asn1)
+        case .uniformResourceIdentifier: try self.init(decodingURI: asn1)
+        case .ipAddress: try self.init(decodingIPAddress: asn1)
+        case .registeredId: try self.init(decodingRegisteredId: asn1)
         }
+    }
+
+    private init(decodingOtherName asn1: ASN1) throws {
+        self = .otherName(try .init(from: asn1))
+    }
+
+    private init(decodingRFC822Name asn1: ASN1) throws {
+        guard let string = asn1.stringValue else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .rfc822Name(string)
+    }
+
+    private init(decodingDNSName asn1: ASN1) throws {
+        guard case .integer(.insane(let bytes)) = asn1.content else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .dnsName(String(decoding: bytes, as: UTF8.self))
+    }
+
+    private init(decodingX400Address asn1: ASN1) throws {
+        self = .x400Address(try .init(from: asn1))
+    }
+
+    private init(decodingDirectoryName asn1: ASN1) throws {
+        // TODO: test why this is nested
+        guard let sequence = asn1.sequenceValue, sequence.count == 1 else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .directoryName(try .init(from: sequence[0]))
+    }
+
+    private init(decodingEDIPartyName asn1: ASN1) throws {
+        self = .ediPartyName(try .init(from: asn1))
+    }
+
+    private init(decodingURI asn1: ASN1) throws {
+        guard let string = asn1.stringIdentifierValue else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .uniformResourceIdentifier(string)
+    }
+
+    private init(decodingIPAddress asn1: ASN1) throws {
+        guard let bytes = asn1.dataValue else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .ipAddress(bytes)
+    }
+
+    private init(decodingRegisteredId asn1: ASN1) throws {
+        guard let bytes = asn1.dataValue else {
+            throw Error.invalidASN1(asn1)
+        }
+        self = .registeredId(.init(rawValue: bytes))
     }
 }
