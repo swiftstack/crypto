eae1a12a8ac2db31bc4e396f24a77e097dd65a26
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-17 22:55:10 +0300

    [Package] Add multiple package sources

--- Package.swift
+++ Package.swift
@@ -4,36 +4,105 @@ import PackageDescription
 let package = Package(
     name: "Crypto",
     products: [
-        .library(name: "SHA1",targets: ["SHA1"]),
-        .library(name: "UUID",targets: ["UUID"]),
-        .library(name: "ASN1",targets: ["ASN1"]),
-        .library(name: "Crypto", targets: ["Crypto"])
+        .library(
+            name: "SHA1",
+            targets: ["SHA1"]),
+        .library(
+            name: "UUID",
+            targets: ["UUID"]),
+        .library(
+            name: "ASN1",
+            targets: ["ASN1"]),
+        .library(
+            name: "Crypto",
+            targets: ["Crypto"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/swift-stack/stream.git",
-            .branch("master")),
-        .package(
-            url: "https://github.com/swift-stack/radix.git",
-            .branch("master")),
-        .package(
-            url: "https://github.com/swift-stack/time.git",
-            .branch("master")),
-        .package(
-            url: "https://github.com/swift-stack/test.git",
-            .branch("master"))
+        .package(name: "Stream"),
+        .package(name: "Radix"),
+        .package(name: "Time"),
+        .package(name: "Test")
     ],
     targets: [
         .target(name: "UInt24"),
-        .target(name: "SHA1", dependencies: ["Hex"]),
-        .target(name: "UUID", dependencies: ["Hex", "SHA1"]),
-        .target(name: "ASN1", dependencies: ["UInt24", "Stream", "Hex"]),
-        .target(name: "X509", dependencies: ["ASN1", "Stream", "Time"]),
-        .target(name: "Crypto", dependencies: ["SHA1", "UUID", "ASN1", "X509"]),
-        .testTarget(name: "SHA1Tests", dependencies: ["Test", "SHA1"]),
-        .testTarget(name: "UUIDTests", dependencies: ["Test", "UUID"]),
-        .testTarget(name: "ASN1Tests", dependencies: ["Test", "ASN1"]),
-        .testTarget(name: "X509Tests", dependencies: ["Test", "X509"]),
-        .testTarget(name: "UInt24Tests", dependencies: ["Test", "UInt24"]),
+        .target(
+            name: "SHA1",
+            dependencies: [
+                .product(name: "Hex", package: "Radix")]),
+        .target(
+            name: "UUID",
+            dependencies: [
+                .product(name: "Hex", package: "Radix"), "SHA1"]),
+        .target(
+            name: "ASN1",
+            dependencies: [
+                "UInt24", "Stream", .product(name: "Hex", package: "Radix")]),
+        .target(
+            name: "X509",
+            dependencies: ["ASN1", "Stream", "Time"]),
+        .target(
+            name: "Crypto",
+            dependencies: ["SHA1", "UUID", "ASN1", "X509"]),
+        .testTarget(
+            name: "SHA1Tests",
+            dependencies: ["Test", "SHA1"]),
+        .testTarget(
+            name: "UUIDTests",
+            dependencies: ["Test", "UUID"]),
+        .testTarget(
+            name: "ASN1Tests",
+            dependencies: ["Test", "ASN1"]),
+        .testTarget(
+            name: "X509Tests",
+            dependencies: ["Test", "X509"]),
+        .testTarget(
+            name: "UInt24Tests",
+            dependencies: ["Test", "UInt24"]),
     ]
 )
+
+// MARK: - custom package source
+
+#if canImport(ObjectiveC)
+import Darwin.C
+#else
+import Glibc
+#endif
+
+extension Package.Dependency {
+    enum Source: String {
+        case local, remote, github
+
+        static var `default`: Self { .local }
+
+        var baseUrl: String {
+            switch self {
+            case .local: return "../"
+            case .remote: return "https://swiftstack.io/"
+            case .github: return "https://github.com/swift-stack/"
+            }
+        }
+
+        func url(for name: String) -> String {
+            return self == .local
+                ? baseUrl + name.lowercased()
+                : baseUrl + name.lowercased() + ".git"
+        }
+    }
+
+    static func package(name: String) -> Package.Dependency {
+        guard let pointer = getenv("SWIFTSTACK") else {
+            return .package(name: name, source: .default)
+        }
+        guard let source = Source(rawValue: String(cString: pointer)) else {
+            fatalError("Invalid source. Use local, remote or github")
+        }
+        return .package(name: name, source: source)
+    }
+
+    static func package(name: String, source: Source) -> Package.Dependency {
+        return source == .local
+            ? .package(name: name, path: source.url(for: name))
+            : .package(name: name, url: source.url(for: name), .branch("dev"))
+    }
+}
